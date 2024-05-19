11578fba402485a01494957c7bff42d9f4ef4f01
Cabernetik2101 <tanya-tihonkova@mail.ru>
2024-05-19 13:13:46 +0300

    added access

--- tatty.py
+++ tatty.py
@@ -3,3 +3,7 @@ print("Мне 25 лет")
 name = int(input("Привет, как твоё имя?: "))
 print("Привет,", name)
 age = int(input("Сколько тебе лет?: "))
+if age > 18:
+    print("Доступ открыт")
+else:
+    print("Доступ закрыт")
